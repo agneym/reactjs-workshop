8341fbc28a5646d9260e868774c380ab9c58999f
Boy with Silver Wings <agney@outlook.in>
2019-06-10 15:53:18 +0530

    move heading to a seperate component

---
+++ src/components/heading.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+
+const Title = styled.h1`
+  font-size: 2rem;
+`
+
+const Subtitle = styled.sub`
+  font-family: ${props => props.theme.fonts.heading};
+  letter-spacing: 0.2rem;
+  font-size: 1rem;
+`
+
+function Heading({ title, subtitle }) {
+  return (
+    <Fragment>
+      <Title>{title}</Title>
+      <Subtitle>{subtitle}</Subtitle>
+    </Fragment>
+  )
+}
+
+Heading.defaultProps = {
+  subtitle: "",
+}
+
+Heading.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
+}
+
+export default Heading
--- src/pages/html.js
+++ src/pages/html.js
@@ -1,26 +1,15 @@
 import React from "react"
-import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Splitter from "../components/splitter"
-
-const Title = styled.h1`
-  font-size: 2rem;
-`
-
-const Subtitle = styled.sub`
-  font-family: ${props => props.theme.fonts.heading};
-  letter-spacing: 0.2rem;
-  font-size: 1rem;
-`
+import Heading from "../components/heading"
 
 function HTMLPage() {
   return (
     <Layout>
       <Splitter>
         <Splitter.Left>
-          <Title>HTML</Title>
-          <Subtitle>Hyper Text Markup Language</Subtitle>
+          <Heading title="HTML" subtitle="Hyper Text Markup Language" />
         </Splitter.Left>
         <Splitter.Right>
           <p>Right</p>
