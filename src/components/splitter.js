494f792917d3013dcd07828bcfdba1b2f7beea6e
Boy with Silver Wings <agney@outlook.in>
2019-06-10 15:49:42 +0530

    setup left side

--- src/components/splitter.js
+++ src/components/splitter.js
@@ -3,14 +3,29 @@ import styled from "styled-components"
 
 const Container = styled.main`
   display: grid;
-  grid-template-columns: 1fr 2fr;
+  grid-template-columns: 1fr 3fr;
   min-height: 100vh;
-  grid-column-gap: 1rem;
+  grid-column-gap: 3rem;
   align-items: center;
 `
 
+const Content = styled.section`
+  height: 100%;
+`
+
+const LeftContent = styled(Content)`
+  text-align: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: ${props => props.theme.colors.primary};
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  color: #ffffff;
+`
+
 function Left({ className, children }) {
-  return <section className={className}>{children}</section>
+  return <LeftContent className={className}>{children}</LeftContent>
 }
 
 function Right({ className, children }) {
--- src/pages/html.js
+++ src/pages/html.js
@@ -1,14 +1,26 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Splitter from "../components/splitter"
+
+const Title = styled.h1`
+  font-size: 2rem;
+`
+
+const Subtitle = styled.sub`
+  font-family: ${props => props.theme.fonts.heading};
+  letter-spacing: 0.2rem;
+  font-size: 1rem;
+`
 
 function HTMLPage() {
   return (
     <Layout>
       <Splitter>
         <Splitter.Left>
-          <h1>HTML</h1>
+          <Title>HTML</Title>
+          <Subtitle>Hyper Text Markup Language</Subtitle>
         </Splitter.Left>
         <Splitter.Right>
           <p>Right</p>
