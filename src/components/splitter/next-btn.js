4acf8138a494c53d8d62cb73fdad835c2e10e7b2
Boy with Silver Wings <agney@outlook.in>
2019-06-12 00:05:16 +0530

    setup next button

---
+++ src/components/splitter/next-btn.js
@@ -0,0 +1,18 @@
+import { Link } from "gatsby";
+import styled from "styled-components";
+
+const NextBtn = styled(Link)`
+  padding: 0.5rem 1.2rem;
+  text-decoration: none;
+  color: ${props => props.theme.colors.primary};
+  border: 0.1rem solid ${props => props.theme.colors.secondary};
+  border-radius: 1.5rem;
+  transition: background-color 0.25s ease-out;
+
+  &:hover {
+    background-color: ${props => props.theme.colors.secondary};
+    color: #fff;
+  }
+`;
+
+export default NextBtn;
--- src/components/splitter/right.js
+++ src/components/splitter/right.js
@@ -3,8 +3,9 @@ import PropTypes from "prop-types";
 import { useSpring, animated } from "react-spring";
 
 import * as S from "./layout";
+import NextBtn from "./next-btn";
 
-function Right({ className, path, children }) {
+function Right({ className, path, children, nextLink }) {
   const appear = useSpring({
     to: { transform: "translateY(0)", opacity: 1 },
     from: { transform: "translateY(-0.4rem)", opacity: 0 },
@@ -16,16 +17,21 @@ function Right({ className, path, children }) {
       <S.Footer>
         <S.FooterContent role="group">
           <p></p>
-          <button>Next</button>
+          {nextLink && <NextBtn>Next ⟶</NextBtn>}
         </S.FooterContent>
       </S.Footer>
     </S.RightContent>
   );
 }
 
+Right.defaultProps = {
+  nextLink: null,
+};
+
 Right.propTypes = {
   className: PropTypes.string,
   path: PropTypes.arrayOf(PropTypes.string),
+  nextLink: PropTypes.string,
 };
 
 export default Right;
