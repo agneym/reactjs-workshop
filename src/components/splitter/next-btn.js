3f1a632971adb857e881f00b8992d1c20a531124
Boy with Silver Wings <agney@outlook.in>
2019-06-20 01:17:04 +0530

    increase the transition time for btn

--- src/components/splitter/next-btn.js
+++ src/components/splitter/next-btn.js
@@ -6,7 +6,7 @@ const NextBtn = styled(Link)`
   text-decoration: none;
   border: 0.1rem solid ${props => props.theme.colors.secondary};
   border-radius: 1.5rem;
-  transition: background-color 0.25s ease-out;
+  transition: background-color 0.5s ease-out;
 
   &:hover {
     background-color: ${props => props.theme.colors.secondary};
